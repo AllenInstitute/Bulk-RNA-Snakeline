a038a0c1ae5143d0257192f214a532c86b3898c7
Beagan Nguy <39427049+beagan-svg@users.noreply.github.com>
2024-04-26 04:39:04 -0700

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=celltypes
-#SBATCH --mem=248g
+#SBATCH --mem=480g
 #SBATCH --time=72:00:00
+#SBATCH --exclusive
+#SBATCH --nodes=2
 
-/usr/bin/time -v snakemake --cores 160 -s main.smk --latency-wait 60 --rerun-incomplete
+/usr/bin/time -v snakemake --cores 176 -s main.smk --latency-wait 60
+#/usr/bin/time -v snakemake --cores 176 -s main.smk --latency-wait 60 --rerun-incomplete
